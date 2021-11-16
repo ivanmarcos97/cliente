d8f3677fa4d8be70451559658b62e4b4aa0926fe
ivanmarcos97 <ivanmarcos97@hotmail.com>
2021-11-16 09:37:57 +0100

    xcv

--- clases/extended-clock.js
+++ clases/extended-clock.js
@@ -1,6 +1,6 @@
 
       class ExtendedClock extends Clock {
-        constructor(template, precision) {
+        constructor(template, precision=1000) {
           super({ template });
           this.precision = precision;
         }
